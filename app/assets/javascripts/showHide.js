1a475ad34accca62b35975550f0aafc51755c2ab
RayMPerry <rmperry09@gmail.com>
2013-09-21 10:16:33 -0500

    Popups work.

--- app/assets/javascripts/showHide.js
+++ app/assets/javascripts/showHide.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
  
         $(".slidingDiv").hide();
+        $(".moreInfo").hide();
+        $("#moreInfoPopup").hide();
         $(".show_hide").show();
+        $(".moreInfoTog").show();
  
     $('.show_hide').click(function(){
     $(".slidingDiv").slideToggle();
     });
+
+    $('#closePop').click(function() {
+  		$("#moreInfoPopup").hide();
+	});
+
+    $('.moreInfoTog').click(function(e){
+    	e.preventDefault();
+    	/*var id = this.id;
+    	$('.moreInfo').not('#' + id).slideUp(400);
+ 		$('div#' + id).slideDown(400);*/
+ 		$("#moreInfoPopup").toggle();
+    });
+
  
 });
