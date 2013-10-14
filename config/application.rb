3ab7b0a6c905d36fb3de6b1756e0416ba44c1eca
Callahan, Sean P (sc023v) <sc023v@att.com>
2013-10-14 10:12:51 -0500

    assets

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module DirectfxRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.assets.precompile += ['*.js', '*.css', '**/*.js', '**/*.css']
+      # config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.js *.css **/*.js **/*.css *-*.png *-*-*.png *-*-*-*.png]
   end
 end
